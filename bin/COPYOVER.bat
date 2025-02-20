cf9038b707af6b65e6cdbc5526c6ef550654b69c
Antti Lukats <Antti.Lukats@googlemail.com>
2025-02-20 12:24:25 +0100

    Create COPYOVER.bat

---
+++ bin/COPYOVER.bat
@@ -0,0 +1,6 @@
+Echo copying sources...
+
+copy ..\ip_repo\src\openmbmc.v ..\ip_repo\Altera\openmbmc.v
+copy ..\ip_repo\src\openmbmc.v ..\ip_repo\AMD\openmbmc.v
+copy ..\ip_repo\src\openmbmc.v ..\ip_repo\Lattice\openmbmc.v
+copy ..\ip_repo\src\openmbmc.v ..\ip_repo\Microchip\openmbmc.v
